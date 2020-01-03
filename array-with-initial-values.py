41c5b933ab6ef9894561948f3a3ac18d54ad3ced
whopriyam <priyambasu16@gmail.com>
2020-01-03 14:10:42 +0530

    Add existing file

---
+++ array-with-initial-values.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+def test_run():
+	print np.empty((5,4), dtype=np.int_)
+	print np.ones((5,4), dtype=np.int_)
+test_run()
---
+++ creating-numpy-array.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+def test_run():
+	print np.array([2,3,4],[5,6,7])
+
