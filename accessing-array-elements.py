0b8f355d8f1e7c8939ae79f58510e249275d9ec4
whopriyam <priyambasu16@gmail.com>
2020-01-03 16:34:09 +0530

    Add existing file

--- accessing-array-elements.py
+++ accessing-array-elements.py
@@ -8,5 +8,9 @@ def test_run():
 	print a[0,1:4]
 	print a[0:2,0:2]
 	print a[:,0:3:2]
-
+	a[0,0] = 1
+	a[1:] = 7
+	print a
+	a[:, 3] = [1,2,3,4,5]
+	print a
 test_run()
---
+++ array-indexing.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def test_run():
+	a = np.random.rand(5)
+	print a
+	indices = np.array([1,1,2,3])
+	print a[indices]
+
+test_run()
